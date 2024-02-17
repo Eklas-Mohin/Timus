d8dc428c69049985aa91111e9b53db64eecb540f
Eklas-Mohin <mohin.030299@gmail.com>
2024-02-17 22:52:44 +0600

    1000. A+B Problem

---
+++ a_plus_b.cpp
@@ -0,0 +1,17 @@
+/**
+ *    author: mohin    
+**/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int a{}, b{}, sum{};
+    cin >> a >> b;
+    sum = a + b;
+    cout << sum << endl;
+    return 0;
+}
