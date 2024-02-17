d9dce68069951ae75c3c061e91e2adb14ccb901d
Eklas-Mohin <mohin.030299@gmail.com>
2024-02-17 21:39:09 +0600

    1293. Eniya

---
+++ eniya.cpp
@@ -0,0 +1,17 @@
+/**
+ *    author: mohin    
+**/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n{}, a{}, b{}, ans{};
+    cin >> n >> a >> b;
+    ans = 2 * n * a * b;
+    cout << ans << endl;
+    return 0;
+}
