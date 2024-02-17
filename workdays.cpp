009c283e4b94a8f67aa8f3625eb08fff3c904b35
Eklas-Mohin <mohin.030299@gmail.com>
2024-02-17 22:17:12 +0600

    1264. Workdays

---
+++ workdays.cpp
@@ -0,0 +1,16 @@
+/**
+ *    author: mohin    
+**/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    long long n{}, m{};
+    cin >> n >> m;
+    cout << n * (m + 1) << endl;
+    return 0;
+}
