958c34f932a60277260f23aad17ef0a8a21d9a24
Eklas-Mohin <mohin.030299@gmail.com>
2024-02-17 21:44:03 +0600

    2012. About Grisha N.

---
+++ about_grisha_n.cpp
@@ -0,0 +1,20 @@
+/**
+ *    author: mohin    
+**/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n{};
+    cin >> n;
+    if (n < 7) {
+        cout << "NO" << endl;
+    } else {
+        cout << "YES" << endl;
+    }
+    return 0;
+}
