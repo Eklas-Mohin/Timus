8dbe46b6f1297e70667c1ad77c5a15a7bd902743
Eklas-Mohin <mohin030299@gmail.com>
2024-10-21 02:25:25 +0600

    Timus 2001. Mathematicians and Berries Solution in C++

--- mathematicians_and_berries.cpp
+++ mathematicians_and_berries.cpp
@@ -1,18 +1,24 @@
-/**
- *    author: mohin    
-**/
-
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    int w1{}, w2{}, w3{}, w4{}, w5{}, w6{}, f1{}, f2{};
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int w1, w2, w3, w4, w5, w6, f1, f2;
     cin >> w1 >> w2 >> w3 >> w4 >> w5 >>  w6;
+
     f1 = w1 - w5;
     f2 = w2 - w4;
+
     cout << f1 << ' ' << f2 << endl;
+    
     return 0;
 }
