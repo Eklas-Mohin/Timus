8d89369e0fd131d2bf405a76fb0a4de4ddf1f7cf
Eklas-Mohin <mohin030299@gmail.com>
2024-10-21 04:00:12 +0600

    Timus 1083. Factorials Solution in C++

--- factorials.cpp
+++ factorials.cpp
@@ -1,22 +1,29 @@
-/**
- *    author: mohin    
-**/
-
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    long long n{}, len{}, ans{1};
-    string str{};
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    long long n, len, ans = 1;
+    string str;
+
     cin >> n >> str;
     len = str.length();
+
     while (n > 0) {
         ans = ans * n;
         n = n - len;
     }
+
     cout << ans << endl;
+
     return 0;
 }
