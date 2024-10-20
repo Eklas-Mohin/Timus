4be97147607456cceff023aa513f5a30a1be89ae
Eklas-Mohin <mohin030299@gmail.com>
2024-10-21 02:31:22 +0600

    Timus 1787. Turn for MEGA Solution in C++

--- turn_for_mega.cpp
+++ turn_for_mega.cpp
@@ -1,24 +1,33 @@
-/**
- *    author: mohin    
-**/
-
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    int k{}, n{}, x{}, arr[100] = {};
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int k, n, x;
+    int arr[100];
+    
     cin >> k >> n;
+
     for (int i = 0; i < n; ++i) {
         cin >> arr[i];
     }
+
     for (int i = 0; i < n - 1; ++i) {
         x = max(0, arr[i] - k);
         arr[i + 1] += x;
     }
+
     arr[n - 1] -= k;
     cout << max(0, arr[n - 1]) << endl;
+
     return 0;
 }
