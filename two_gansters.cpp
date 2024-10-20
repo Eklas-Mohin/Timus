085c030bc7207972bede7937c1534f987eded1be
Eklas-Mohin <mohin030299@gmail.com>
2024-10-21 02:04:10 +0600

    Timus 1409. Two Gangsters Solution in C++

--- two_gansters.cpp
+++ two_gansters.cpp
@@ -1,16 +1,21 @@
-/**
- *    author: mohin    
-**/
-
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    int a{}, b{};
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int a, b;
     cin >> a >> b;
+
     cout << b - 1 << ' ' << a - 1 << endl;
+
     return 0;
 }
