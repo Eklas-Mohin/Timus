fc56e6e4593f0229a1ec8e459db430b0be96e56f
Eklas-Mohin <mohin030299@gmail.com>
2024-10-21 02:59:24 +0600

    Timus 1639. Chocolate 2 Solution in C++

--- chocolate_2.cpp
+++ chocolate_2.cpp
@@ -1,21 +1,27 @@
-/**
- *    author: mohin    
-**/
-
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    int a{}, b{};
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int a, b;
     cin >> a >> b;
+
     a *= b;
+    
     if (a % 2) {
         cout << "[second]=:]" << endl;
     } else {
         cout << "[:=[first]" << endl;
     }
+
     return 0;
 }
