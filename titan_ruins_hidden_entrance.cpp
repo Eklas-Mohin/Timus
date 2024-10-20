2fa8dd77260cf4f14d56da3726da33b9464bbf36
Eklas-Mohin <mohin030299@gmail.com>
2024-10-21 03:04:03 +0600

    Timus 1910. Titan Ruins: Hidden Entrance Solution in C++

--- titan_ruins_hidden_entrance.cpp
+++ titan_ruins_hidden_entrance.cpp
@@ -1,19 +1,26 @@
-/**
- *    author: mohin    
-**/
-
-#include <bits/stdc++.h>
+#include <iostream>
+#include <climits>
 
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    int n{}, mx{INT_MIN}, p{}, arr[1000] = {};
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int n, mx, p, arr[1000];
+    mx = INT_MIN;
+
     cin >> n;
     for (int i = 0; i < n; ++i) {
         cin >> arr[i];
     }
+
     for (int i = 2; i < n; ++i) {
         int sum = arr[i] + arr[i - 1] + arr[i - 2];
         if (sum > mx) {
@@ -21,6 +28,8 @@ int main() {
             p = i;
         }
     }
+
     cout << mx << ' ' << p << endl;
+
     return 0; 
 }
