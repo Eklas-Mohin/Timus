a4bffd792b4d5a9cd1e27011bdb4339f1affe66a
Eklas-Mohin <mohin030299@gmail.com>
2024-10-21 03:09:07 +0600

    Timus 1068. Sum Solution in C++

--- sum.cpp
+++ sum.cpp
@@ -1,20 +1,25 @@
-/**
- *    author: mohin    
-**/
-
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    int n{};
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int n;
     cin >> n;
+    
     if (n > 0) {
         cout << (n * (n + 1)) / 2 << endl;
     } else {
-        cout << 1 - ((-n * -(n - 1)) / 2) << endl;
+        cout << 1 + ((n * (1 - n)) / 2) << endl;
     }
+
     return 0;
 }
