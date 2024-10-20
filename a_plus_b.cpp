dae93ffde917f21bcf1f64de6b535ceaa49a82c7
Eklas-Mohin <mohin030299@gmail.com>
2024-10-21 02:06:47 +0600

    Timus 1000. A + B Solution in C++

--- a_plus_b.cpp
+++ a_plus_b.cpp
@@ -1,17 +1,23 @@
-/**
- *    author: mohin    
-**/
-
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    int a{}, b{}, sum{};
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int a, b;
+    
     cin >> a >> b;
-    sum = a + b;
+
+    int sum = a + b;
     cout << sum << endl;
+
     return 0;
 }
