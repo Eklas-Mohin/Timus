521650be104704671931fe129fd8de747f608db5
Eklas-Mohin <mohin030299@gmail.com>
2024-10-21 02:16:36 +0600

    Timus 1877. Bicycle Codes Solution in C++

--- bicycle_codes.cpp
+++ bicycle_codes.cpp
@@ -1,20 +1,25 @@
-/**
- *    author: mohin    
-**/
-
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    int n{}, m{};
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int n, m;
     cin >> n >> m;
-    if (n % 2 == 0 or m % 2 == 1) {
+
+    if (n % 2 == 0 || m % 2 == 1) {
         cout << "yes" << endl;
     } else {
         cout << "no" << endl;
     }
+
     return 0;
 }
