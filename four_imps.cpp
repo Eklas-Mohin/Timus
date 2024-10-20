8bf1c7da90e815459eacceb93b1a7818027a823e
Eklas-Mohin <mohin030299@gmail.com>
2024-10-21 03:13:00 +0600

    Timus 1924. Four Imps Solution in C++

--- four_imps.cpp
+++ four_imps.cpp
@@ -1,21 +1,27 @@
-/**
- *    author: mohin    
-**/
-
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    int n{};
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int n;
     cin >> n;
+
     n = (n * (n + 1)) / 2;
+
     if (n % 2) {
         cout << "grimy" << endl;
     } else {
         cout << "black" << endl;
     }
+    
     return 0; 
 }
