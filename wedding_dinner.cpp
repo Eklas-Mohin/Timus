9c8f00a5c1abdc8031cae4809675509599bb61cd
Eklas-Mohin <mohin030299@gmail.com>
2024-10-21 02:48:35 +0600

    Timus 2100. Wedding Dinner Solution in C++

--- wedding_dinner.cpp
+++ wedding_dinner.cpp
@@ -1,18 +1,24 @@
-/**
- *    author: mohin    
-**/
-
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    int n{}, ans{};
-    string str{};
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int n, ans;
+    string str;
+
     cin >> n;
     ans = n + 2;
+
     for (int i = 0; i < n; ++i) {
         cin >> str;
         for (int i = 0; i < str.length(); ++i) {
@@ -22,9 +28,12 @@ int main() {
             }
         }
     }
+
     if (ans == 13) {
         ans += 1;
     }
+    
     cout << ans * 100 << endl;
+
     return 0;
 }
