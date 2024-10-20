53b6fa781fec93ce1704514f553b2a59dd030e98
Eklas-Mohin <mohin030299@gmail.com>
2024-10-21 01:59:35 +0600

    Timus 1293. Eniya Solution in C++

--- eniya.cpp
+++ eniya.cpp
@@ -1,17 +1,22 @@
-/**
- *    author: mohin    
-**/
-
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    int n{}, a{}, b{}, ans{};
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int n, a, b;
     cin >> n >> a >> b;
-    ans = 2 * n * a * b;
+    
+    int ans = 2 * n * a * b;
     cout << ans << endl;
+
     return 0;
 }
