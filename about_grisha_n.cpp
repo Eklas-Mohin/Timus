c293a48084f174c0b77cfc05d7164f4f683a8d3d
Eklas-Mohin <mohin030299@gmail.com>
2024-10-21 02:13:41 +0600

    Timus 2012. About Grisha N. Solution in C++

--- about_grisha_n.cpp
+++ about_grisha_n.cpp
@@ -1,20 +1,25 @@
-/**
- *    author: mohin    
-**/
-
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    int n{};
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int n;
     cin >> n;
+
     if (n < 7) {
         cout << "NO" << endl;
     } else {
         cout << "YES" << endl;
     }
+
     return 0;
 }
