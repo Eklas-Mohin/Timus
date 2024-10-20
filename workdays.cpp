19121530e0c7186c79d83d188fa440869e8ef574
Eklas-Mohin <mohin030299@gmail.com>
2024-10-21 02:21:17 +0600

    Timus 1264. Workdays Solution in C++

--- workdays.cpp
+++ workdays.cpp
@@ -1,16 +1,21 @@
-/**
- *    author: mohin    
-**/
-
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    long long n{}, m{};
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    long long n, m;
     cin >> n >> m;
+
     cout << n * (m + 1) << endl;
+
     return 0;
 }
